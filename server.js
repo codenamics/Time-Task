62dfb0d0b82206f5da46387af1cd54b497056e65
codenamics <info@codenamics.pl>
2019-02-18 16:09:50 +0100

    add

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/tasks', tasks)
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
 const port = process.env.PORT || 4000
 
 app.listen(port, () => {
