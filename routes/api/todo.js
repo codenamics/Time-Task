14efcb26af723a93e86fd6573664bb163986221b
codenamics <info@codenamics.pl>
2019-03-16 00:34:37 +0100

    up

--- routes/api/todo.js
+++ routes/api/todo.js
@@ -1,103 +1,134 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('passport')
-const validateMonthInput = require('../../validation/month')
-const Todo = require('../../models/Todo')
-
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const validateMonthInput = require("../../validation/month");
+const Todo = require("../../models/Todo");
 
 //Create new todo
 
-router.post('/', passport.authenticate('jwt', {
+router.post(
+  "/",
+  passport.authenticate("jwt", {
     session: false
-}), (req, res) => {
-    const {
-        todoTitle,
-        todoDescription,
-        status
-    } = req.body
+  }),
+  (req, res) => {
+    const { todoTitle, todoDescription, status } = req.body;
 
     const newTodo = new Todo({
-        todoTitle,
-        todoDescription,
-        status,
-        user: req.user.id
-    })
-    newTodo.save()
-        .then(todo => {
-            res.json(todo)
-        }).catch(err => console.log(err))
-})
+      todoTitle,
+      todoDescription,
+      status,
+      user: req.user.id
+    });
+    newTodo
+      .save()
+      .then(todo => {
+        res.json(todo);
+      })
+      .catch(err => console.log(err));
+  }
+);
 
 //Get all tasks of current login user
 
-router.get('/', passport.authenticate('jwt', {
+router.get(
+  "/",
+  passport.authenticate("jwt", {
     session: false
-}), (req, res) => {
+  }),
+  (req, res) => {
     Todo.find({
-            user: req.user.id
-        }).sort({
-            date: -1
+      user: req.user.id
+    })
+      .sort({
+        date: -1
+      })
+      .then(todo => {
+        res.json(todo);
+      })
+      .catch(err =>
+        res.status(404).json({
+          notasks: "No todos found"
         })
-        .then(todo => {
-            res.json(todo)
-        }).catch(err => res.status(404).json({
-            notasks: "No todos found"
-        }))
-})
+      );
+  }
+);
 
 //Get all todos by current logged in user
-router.get('/', passport.authenticate('jwt', {
+router.post(
+  "/search",
+  passport.authenticate("jwt", {
     session: false
-}), (req, res) => {
+  }),
+  (req, res) => {
     Todo.find({
-            user: req.user.id,
+      user: req.user.id,
 
-        }).sort({
-            date: -1
-        })
-        .then(todos => {
-            res.json(todos)
-        }).catch(err => res.status(404).json({
+      todoDescription: { $all: req.body.search }
+    })
+      .then(todos => {
+        if (todos.length === 0) {
+          res.status(404).json({
             notasks: "No todos found"
-        }))
-})
-
+          });
+        }
+        res.json(todos);
+      })
+      .catch(err =>
+        res.status(404).json({
+          notasks: "No todos found"
+        })
+      );
+  }
+);
 
 //Delete user todo
 
-router.delete('/:id', passport.authenticate('jwt', {
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", {
     session: false
-}), (req, res) => {
+  }),
+  (req, res) => {
     Todo.findOne({
-            user: req.user.id,
-            _id: req.params.id
+      user: req.user.id,
+      _id: req.params.id
+    })
+      .then(todo => {
+        todo.remove().then(() => {
+          res.json({
+            success: true
+          });
+        });
+      })
+      .catch(err =>
+        res.status(404).json({
+          notask: "There is not todo with that ID to be deleted"
         })
-        .then(todo => {
-            todo.remove()
-                .then(() => {
-                    res.json({
-                        success: true
-                    })
-                })
-        }).catch(err => res.status(404).json({
-            notask: 'There is not todo with that ID to be deleted'
-        }))
-})
+      );
+  }
+);
 
 //Get month by ID
-router.get('/:id', passport.authenticate('jwt', {
+router.get(
+  "/:id",
+  passport.authenticate("jwt", {
     session: false
-}), (req, res) => {
+  }),
+  (req, res) => {
     Todo.findOne({
-            user: req.user.id,
-            _id: req.params.id
+      user: req.user.id,
+      _id: req.params.id
+    })
+      .then(todo => {
+        res.json(todo);
+      })
+      .catch(err =>
+        res.status(404).json({
+          notask: "There is not task with that ID to be deleted"
         })
-        .then(todo => {
-            res.json(todo)
-        }).catch(err => res.status(404).json({
-            notask: 'There is not task with that ID to be deleted'
-        }))
-})
+      );
+  }
+);
 
-
-module.exports = router
+module.exports = router;
